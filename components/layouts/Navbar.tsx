5ce8c6db14f87574fc849163ae4e6d02ea3d5111
smit <smitkumar.patel@tatvasoft.com>
2025-02-10 11:58:34 +0530

    added home page UI and Navbar changes

--- components/layouts/Navbar.tsx
+++ components/layouts/Navbar.tsx
@@ -6,7 +6,7 @@ interface NavbarProps {
 const Navbar: FC<NavbarProps> = ({ user }) => {
     return (
         <header className="sticky top-0 z-50 w-full border-b bg-background py-3">
-            <nav className="container px-2 sm:px-4 lg:px-6 flex items-center justify-between"></nav>
+            <nav className="container px-2 sm:px-4 lg:px-6 flex items-center justify-between">NavBar</nav>
         </header>
     );
 };
