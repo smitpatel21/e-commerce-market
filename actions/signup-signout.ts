483514f1c01eaf13e07d2ffd3e21a023d2309c2d
Smit Patel <smitpatel21.in@gmail.com>
2025-03-09 20:55:13 +0530

    formatted code

--- actions/signup-signout.ts
+++ actions/signup-signout.ts
@@ -1,25 +1,19 @@
 "use server";
 
-
 import { signUpSchema } from "@/lib/validators/signup-signout-schema";
 
 const signup = (formData: FormData) => {
+  const name = formData.get("name") as string;
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
 
-
-    const name = formData.get("name") as string;
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-
-     fetch("http://localhost:3000/api/auth/signup", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ name, email, password }),
-    });
-
-
-
+  fetch("http://localhost:3000/api/auth/signup", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, email, password }),
+  });
 };
 
 export { signup };
